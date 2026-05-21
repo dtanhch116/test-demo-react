08256588dc614b42648d6543f52d4fcc91fc36e1
Dinh Tuan Anh <dtanhch116@gmail.com>
2026-05-21 12:46:04 +0700

    react StateFul and Stateless component

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -2,45 +2,62 @@ import React from "react";
 import "./DisplayInfo.scss";
 import logo from "../logo.svg";
 
-class DisplayInfo extends React.Component {
+// class DisplayInfo extends React.Component {
 
-    state = {
-        isShowListUser: true
+//     handleClickDelete = (userId) => {
+//         this.props.onDeleteUser(userId);
+//     }
+
+//     render() {
+//         console.log("call me render");
+
+//         const { listUsers } = this.props;
+
+//         return (
+//             <div className="display-info-container">
+//                 {true &&
+//                     <>
+//                         {listUsers.map((user, index) => {
+//                             return (
+//                                 <div key={user.id}>
+//                                     <p className={user.age > 18 ? "red" : "green"}>
+//                                         {user.id} - {user.name} - {user.age}
+//                                     </p>
+//                                     <button onClick={() => this.handleClickDelete(user.id)}>Delete</button>
+//                                 </div>
+//                             )
+//                         })}
+//                     </>}
+//             </div>
+//         );
+//     }
+// }
+
+const DisplayInfo = (props) => {
+
+    const handleClickDelete = (userId) => {
+        props.onDeleteUser(userId);
     }
 
-    handleClickButton = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        });
-    }
+    const { listUsers } = props;
 
-    handleClickDelete = (userId) => {
-        this.props.onDeleteUser(userId);
-    }
-
-    render() {
-        const { listUsers } = this.props;
-
-        return (
-            <div className="display-info-container">
-                <img src={logo} alt="logo" />
-                <button onClick={() => this.handleClickButton()}>{this.state.isShowListUser ? "Hide Users" : "Show Users"}</button>
-                {this.state.isShowListUser &&
-                    <>
-                        {listUsers.map((user, index) => {
-                            return (
-                                <div key={user.id}>
-                                    <p className={user.age > 18 ? "red" : "green"}>
-                                        {user.id} - {user.name} - {user.age}
-                                    </p>
-                                    <button onClick={() => this.handleClickDelete(user.id)}>Delete</button>
-                                </div>
-                            )
-                        })}
-                    </>}
-            </div>
-        );
-    }
+    return (
+        <div className="display-info-container">
+            {true &&
+                <>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <div key={user.id}>
+                                <p className={user.age > 18 ? "red" : "green"}>
+                                    {user.id} - {user.name} - {user.age}
+                                </p>
+                                <button onClick={() => handleClickDelete(user.id)}>Delete</button>
+                            </div>
+                        )
+                    })}
+                </>}
+        </div>
+    );
 }
 
 export default DisplayInfo;
