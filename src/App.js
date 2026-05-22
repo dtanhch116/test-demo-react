feb382bb992daaf4fc679fef440f3fa6022020e3
Dinh Tuan Anh <dtanhch116@gmail.com>
2026-05-22 12:47:37 +0700

    Điều hướng trang vs Links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header/Header';
+import User from './components/User/User';
+import Admin from './components/Admin/Admin';
 
+import { Link } from 'react-router-dom';
 
 const App = () => {
 
@@ -9,6 +12,17 @@ const App = () => {
 
     <div className="app-container">
       <Header />
+      <div>
+        test link
+        <div>
+          <button className="">
+            <Link to="/users">User</Link>
+          </button>
+          <button className="">
+            <Link to="/admins">Admin</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
